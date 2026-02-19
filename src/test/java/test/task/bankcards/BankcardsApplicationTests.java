2e5830d5c6d0f269e55680c887dcd57dd2178570
veqluw <mzaboronok@icloud.com>
2026-02-19 18:09:30 +0300

    initial commit

---
+++ src/test/java/test/task/bankcards/BankcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package test.task.bankcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
