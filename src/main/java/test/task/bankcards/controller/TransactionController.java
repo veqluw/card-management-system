822544eeaf53554dd378622bfa5166abccd15b87
veqluw <mzaboronok@icloud.com>
2026-02-26 18:50:41 +0300

    add transaction controller

---
+++ src/main/java/test/task/bankcards/controller/TransactionController.java
@@ -0,0 +1,28 @@
+package test.task.bankcards.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import test.task.bankcards.dto.request.TransferRequest;
+import test.task.bankcards.service.TransactionService;
+
+@RestController
+@RequestMapping("/api/transactions")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> transfer(@RequestBody @Valid TransferRequest request) {
+        transactionService.transfer(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
